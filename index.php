1422311014a66895b3b5e4e3e044fe8fa7d9cce4
Wathiq Al-Qajar <wathiq-iq@users.noreply.github.com>
2016-10-08 02:23:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ ini_set("error_log", __DIR__."/error_log");
 require 'farsiGD.php';
 $gd = new FarsiGD();
 ob_start();
-define('API_KEY','XXX:XXX');// your API KEY here ...
+define('API_KEY','268863964:AAHDYM_lpUTFMvZHJE6E_qNURYLqp9l7Hdg');// your API KEY here ...
 $update = json_decode(file_get_contents('php://input'));
 function makeHTTPRequest($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
